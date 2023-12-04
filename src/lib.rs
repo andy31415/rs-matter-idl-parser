7aca4bfd8d3f118cf72e4562e99bff91aedc7871
Andrei Litvin <andy314@gmail.com>
2023-12-04 10:24:16 -0500

    Added some tests for a very very basic parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,54 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use nom::{branch::alt, bytes::complete::tag, combinator::map, IResult};
+use nom_locate::LocatedSpan;
+
+// easier to type and not move str around
+type Span<'a> = LocatedSpan<&'a str>;
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub enum ApiMaturity {
+    STABLE,
+    PROVISIONAL,
+    INTERNAL,
+    DEPRECATED,
 }
+
+pub fn parse_api_maturity(span: Span) -> IResult<Span, ApiMaturity> {
+    // This actually cannot fail
+    let specified: IResult<Span, ApiMaturity> = alt((
+        map(tag("stable"), |_| ApiMaturity::STABLE),
+        map(tag("provisional"), |_| ApiMaturity::PROVISIONAL),
+        map(tag("internal"), |_| ApiMaturity::INTERNAL),
+        map(tag("deprecated"), |_| ApiMaturity::DEPRECATED),
+    ))(span);
+
+    if specified.is_err() {
+        // Do not consume anything, return stable if not specified
+        return Ok((span, ApiMaturity::STABLE));
+    }
+
+    specified
+}
+
+// TODO:
+// constant_entry: [maturity] id "=" positive_integer ";"
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    
+    fn remove_loc<O>(src: IResult<Span, O>) -> IResult<Span, O> {
+        src.map(|(span, o)| {
+            ((*span.fragment()).into(), o)
+        })
+    }
+
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_parse_maturity() {
+        assert_eq!(parse_api_maturity("123".into()), Ok(("123".into(), ApiMaturity::STABLE)));
+
+        assert_eq!(remove_loc(parse_api_maturity("stable abc".into())), Ok((" abc".into(), ApiMaturity::STABLE)));
+        assert_eq!(remove_loc(parse_api_maturity("provisional abc".into())), Ok((" abc".into(), ApiMaturity::PROVISIONAL)));
     }
 }

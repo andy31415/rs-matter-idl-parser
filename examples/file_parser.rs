ce4035583042d407409f1ec33b7ee36995dfece9
Andrei Litvin <andy314@gmail.com>
2023-12-06 06:26:21 -0500

    Prepare for better flamegraph output using repeats

--- examples/file_parser.rs
+++ examples/file_parser.rs
@@ -9,14 +9,20 @@ use rs_matter_idl_parser::Idl;
 struct Args {
     /// IDL file to open
     name: String,
+    
+    /// How many time to parse this file
+    #[arg(short,long,value_name="CNT")]
+    repeat_count: Option<u32>,
 }
 
 fn main() -> miette::Result<()> {
     let args = Args::parse();
-    
+
     let contents = fs::read_to_string(args.name)
         .expect("Valid input file");
     
-    Idl::parse((&*contents).into())?;
+    for _ in 0..args.repeat_count.unwrap_or(1) {
+        Idl::parse((&*contents).into())?; 
+    }
     Ok(())
 }

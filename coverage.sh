9aa3962355669b606ecf53c5cb32dde938dfdea1
Andrei Litvin <andreilitvin@google.com>
2023-12-07 15:39:07 -0500

    Tarpaulin works with less setup. try to use it

--- coverage.sh
+++ coverage.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
+
+gcov_coverage() {
 # need nightly for -Zprofile
 rustup default nightly
-
 export CARGO_INCREMENTAL=0
 export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
 export RUSTDOCFLAGS="-Cpanic=abort"
-
 cargo build --verbose $CARGO_OPTIONS
 cargo test --verbose $CARGO_OPTIONS
 
@@ -14,14 +14,35 @@ cargo test --verbose $CARGO_OPTIONS
 # cargo run                \
 #    --example file_parser \
 #    -- /usr/local/google/home/andreilitvin/devel/connectedhomeip/examples/all-clusters-app/all-clusters-common/all-clusters-app.matter
-
-
 # Actual coverage
+
 mkdir -p target/coverage
 grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/coverage/
 
-# codecov.io version
-#      zip -0 ccov.zip `find . \( -name "YOUR_PROJECT_NAME*.gc*" \) -print`;
-#      ./grcov ccov.zip -s . -t lcov --llvm --branch --ignore-not-existing --ignore "/*" -o lcov.info;
-#      bash <(curl -s https://codecov.io/bash) -f lcov.info;
+}
+
+tarpaulin_coverage(){
+    cargo tarpaulin -o Html --output-dir target/coverage
+}
+
+case $1 in
+  help)
+      echo "Usage: "
+      echo "  $0 [grcov,tarpaulin]"
+      ;;
+  grcov)
+      echo "GRCOV coverage"
+      gcode_coverage
+      echo "output in target/coverage/index.html"
+      ;;
+  tarpaulin)
+      echo "tarpaulin coverage"
+      tarpaulin_coverage
+      echo "output in target/coverage/tarpaulin-report.html"
+  ;;
+  *)
+    tarpaulin_coverage;
+  ;;
+
+esac
 
